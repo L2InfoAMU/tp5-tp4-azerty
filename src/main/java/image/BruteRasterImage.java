9dbfd1ba77e3f0010a80cf76fc2793df23684287
zarouali <anaszarouali9@gmail.com>
2018-11-30 10:38:28 +0100

    Version 1.0
    Creation BruteRasterImage

---
+++ src/main/java/image/BruteRasterImage.java
@@ -0,0 +1,21 @@
+package image;
+
+import image.Image;
+import javafx.scene.paint.Color;
+
+public class BruteRasterImage implements Image {
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+}
