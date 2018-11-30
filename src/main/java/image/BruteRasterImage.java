75952ea8d47be8fa11ffaa8576a62e8b3790cc25
zarouali <anaszarouali9@gmail.com>
2018-11-30 11:02:25 +0100

    Version 1.1
    — public BruteRasterImage(Color color, int width, int height)

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -4,6 +4,29 @@ import image.Image;
 import javafx.scene.paint.Color;
 
 public class BruteRasterImage implements Image {
+
+    Color color;
+    int width;
+    int height;
+    Color [][] pixels;
+
+    public BruteRasterImage(Color color, int width, int height){
+        this.width=width;
+        this.height=height;
+         for(int i=0; i<width; i++){
+             for(int j=0;j<height; j++){
+                 pixels[i][j]=color;
+             }
+         }
+
+    }
+
+    public BruteRasterImage(Color[][] colors){
+
+
+    }
+
+
     @Override
     public Color getPixelColor(int x, int y) {
         return null;
