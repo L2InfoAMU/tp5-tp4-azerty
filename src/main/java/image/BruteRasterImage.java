fabe357c91f3a3b5b34855240542747cc458b14d
zarouali <anaszarouali9@gmail.com>
2018-11-30 11:52:20 +0100

    Version 1.3
    LES NEUFS

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -25,40 +25,60 @@ public class BruteRasterImage implements Image {
 
 
     }
-    public void createRepresentation(){ //: alloue la matrice représentant l’image.
-        pixels=new Color [this.width][this.height];
+
+    public void createRepresentation() { //: alloue la matrice représentant l’image.
+        pixels = new Color[this.width][this.height];
     }
+
     public void setPixelColor(Color color, int x, int y) // : fixe la couleur d’un pixel.
     {
-        pixels[x][y]= color;
+        pixels[x][y] = color;
 
 
     }
+
     private void setPixelsColor(Color[][] pixels)// met à jour les valeurs de couleurs de l’image en utilisant les valeurs de la matrice donnée en paramètre.
-    {}
+    {
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                this.pixels[i][j] = pixels[i][j];
+            }
+        }
+    }
+
     private void setPixelsColor(Color color) //change les valeurs de tous les pixels pour qu’ils soient tous de la couleur donnée en paramètre.
-    {}
+    {
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                this.pixels[i][j] = color;
+            }
+        }
+    }
+
+    @Override
     public int getWidth() //retourne la largeur de l’image.
-    {}
+    {
+        return this.width;
+    }
+    @Override
     public int getHeight()//retourne la hauteur de l’image.
-    {}
+    {
+        return this.height;
+    }
+
     protected void setWidth(int width) //: fixe la largeur de l’image.
-    { this.width=width; }
+    {
+        this.width = width;
+    }
+
     protected void setHeight(int height)// : fixe la hauteur de l’image.
-    { this.height=height; }
+    {
+        this.height = height;
+    }
 
     @Override
     public Color getPixelColor(int x, int y) {
-        return null;
+        return this.pixels[x][y];
     }
 
-    @Override
-    public int getWidth() {
-        return 0;
-    }
-
-    @Override
-    public int getHeight() {
-        return 0;
-    }
 }
