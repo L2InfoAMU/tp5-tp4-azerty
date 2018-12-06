99f41de9423a0e36761e827d747eee2932fe9f95
BlackFox3000 <flo-ti@hotmail.fr>
2018-12-06 10:27:53 +0100

    Version 3.9
    Exo 3 fini !

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -47,8 +47,8 @@ public class PaletteRasterImage implements Image {
     public void setPixelsColor(Color[][] pixels )                         //: met à jour les valeurs de couleurs de l’image en utilisant les valeurs de la matrice donnée en paramètre.
     {
         requiresNonNull(pixels);
-        for(int x=0;x<getHeight();x++){
-          for(int y=0; y<getWidth() ;y++){
+        for(int x=0;x<getWidth();x++){
+          for(int y=0; y<getHeight() ;y++){
               setPixelColor(pixels[x][y],x,y);
           }
          }
