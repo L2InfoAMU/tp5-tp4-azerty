97925a775a279f96d494a5cb58ccd76a12c8f33a
BlackFox3000 <flo-ti@hotmail.fr>
2018-12-09 22:05:40 +0100

    Version 6.1
    Plus qu'a faire le constructeur et getPixelColor

---
+++ src/main/java/image/VectorImage.java
@@ -0,0 +1,46 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class VectorImage implements Image{
+    List<Shape> shapes;
+    int width, height;
+
+    public VectorImage(List<Shape> shapes, int width, int height){
+    this.shapes=shapes;
+    this.width=width;
+    this.height=height;
+
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return ;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    protected void setWidth(int width)           // : fixe la largeur de l’image.
+    { this.width=width; }
+    protected void setHeight(int height)         // : fixe la hauteur de l’image.
+    { this.height=height; }
+}
+
+
+/*
+— VectorImage : dans cette représentation, on stocke une liste de formes géométriques (utilisant l’interface
+Shape). Une image de type sera construite à partir de List<Shape> shapes. Chaque forme géométrique
+(qui peut être un cercle, un rectangle, . . .) contient un certain nombre de points et définit la couleur des
+points à l’intérieur. La couleur d’un point de l’image est la couleur de la première forme dans la liste qui
+contient le point ou bien blanc si le point n’est contenu dans aucune forme.
+ */
